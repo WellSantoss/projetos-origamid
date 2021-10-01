9be524e5b5ed18d9f0048ac7b0be52fa59153352
Wellington Santos <wellington1998santoss@gmail.com>
2021-10-01 20:46:59 -0300

    first commit

---
+++ bikcraft/js/script.js
@@ -0,0 +1,20 @@
+if (window.SimpleSlide){
+  	new SimpleSlide({
+      	slide: "quote", // nome do atributo data-slide="principal"
+      	auto: true, // se o slide deve passar automaticamente
+      	time: 5000, // tempo de transição dos slides
+      	pauseOnHover: true // pausa a transição automática
+    });
+  
+	new SimpleSlide({
+		slide: "portifolio", // nome do atributo data-slide="principal"
+		auto: true, // se o slide deve passar automaticamente
+		nav: true, // se deve ou não mostrar a navegação
+		time: 5000, // tempo de transição dos slides
+		pauseOnHover: true // pausa a transição automática
+    });
+}
+
+if (window.SimpleAnime){
+  	new SimpleAnime();
+}
